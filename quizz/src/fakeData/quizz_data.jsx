6a15db32db1cec3dc7285997838a81262d4e6f5c
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-04 22:36:41 +0200

    added starting components

---
+++ quizz/src/fakeData/quizz_data.jsx
@@ -0,0 +1,23 @@
+[
+    {
+      id: 1,
+      title: "General Knowledge Quiz",
+      questions: [
+        {
+          id: 1,
+          type: "multiple-choice",
+          question: "What is the capital of England?",
+          options: ["Paris", "London", "Berlin", "Madrid"],
+          answer: "London",
+          points: 10,
+        },
+        {
+          id: 2,
+          type: "input",
+          question: "What is 10 + 3?",
+          answer: "13",
+          points: 5,
+        },
+      ],
+    },
+  ];
---
+++ quizz/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, useContext} from "react";
+import axios from "axios";
+import "../styles/leftPannel.css"
+import { useNavigate } from 'react-router-dom';
+
+
+const Home = ()=>{
+
+
+
+    return(
+        
+
+    <div>
+ 
+    </div>
+  );
+};
+
+export default Home;
