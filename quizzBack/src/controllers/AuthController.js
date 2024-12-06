11b11381fbd852d4a08415b671edcac678517901
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-06 21:56:38 +0200

    added controllers

---
+++ quizzBack/src/controllers/AuthController.js
@@ -0,0 +1,3 @@
+import { Users } from "../models/users.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
---
+++ quizzBack/src/controllers/usersController.js
@@ -0,0 +1,20 @@
+import { Users } from "../models/users.model.js";
+import { throwError, throwNotFound } from "../utils/errors.js";
+
+
+export const getUser = async (req,res) => {
+    const id = req.params.id;
+
+
+    try{
+
+
+
+
+    }catch (error){
+        console.error(throwError);
+
+
+    }
+
+}
