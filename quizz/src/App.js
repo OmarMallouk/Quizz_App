7f88cb4fca0486f9a9a6e823ab23a14a472a47d4
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-10 01:07:50 +0200

    added a new route

--- quizz/src/App.js
+++ quizz/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
-
+import Login from './pages/login';
 
 function App() {
   return (
     <div className="App">
        <BrowserRouter>
           <Routes>
+          <Route path="/login" element={<Login/>}/>
           <Route path='/' element={<Home/>} />
           <Route path='/quiz' element={<Quiz/>} />
  
