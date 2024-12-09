8d281f6aca67d1cd370939214faec0682a1644a9
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-09 23:58:26 +0200

    added more in the auth controller

--- quizzBack/src/controllers/AuthController.js
+++ quizzBack/src/controllers/AuthController.js
@@ -1,3 +1,38 @@
 import { Users } from "../models/users.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+
+
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+  
+    try {
+      const user = await Users.findOne({
+        username,
+      });
+  
+      if (!user) {
+        return res.status(404).send({
+          message: "Invalid Credentials",
+        });
+      }
+  
+      const check = await bcrypt.compare(password, user.password);
+  
+      if (!check) {
+        return res.status(400).send({
+          message: "Invalid Credentials",
+        });
+      }
+  
+      const token = await jwt.sign({ userId: user.id }, "secret");
+  
+      return res.status(200).send({ user, token });
+    } catch (error) {
+      console.log(error.message);
+  
+      return res.status(500).send({
+        message: "Something went wrong",
+      });
+    }
+  };
