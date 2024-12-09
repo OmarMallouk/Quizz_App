1d564737ff521137713f9d197e2c1e695976cc95
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-10 00:39:32 +0200

    added another hook

--- quizzBack/src/index.js
+++ quizzBack/src/index.js
@@ -2,13 +2,16 @@ import express from "express";
 import connectToDatabase from "./base/connection.js";
 import userRoutes from "./routes/users.routes.js";
 import quizRoutes from "./routes/quiz.routes.js"
+import authRoutes from "./routes/auth.routes.js"
 
 
 const app = express();
 
 app.use(express.json());
+app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/quiz", quizRoutes);
+
 
 
 app.listen(8080, async () => {
